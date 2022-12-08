a95842923498a38408ff64db165ce5df78a49a7b
Lecser <107482432+Lecser@users.noreply.github.com>
2022-12-08 09:30:26 +0300

    fix

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_APP_API_URL,
+  baseURL: import.meta.env.VITE_APP_API_URL.PROD,
 });
 
 api.interceptors.request.use((config) => {
   config.url =
     config.url +
-    `&appid=${import.meta.env.VITE_APP_API_KEY}&lang=ru&units=metric`;
+    `&appid=${import.meta.env.VITE_APP_API_KEY.PROD}&lang=ru&units=metric`;
   return config;
 });
